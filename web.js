568138bbdb4b14e1d6a9cdd53c6dde1a929754a3
Ubuntu <ubuntu@ip-172-31-21-22.us-west-2.compute.internal>
2013-07-12 17:38:36 +0000

    Read from html test3

--- web.js
+++ web.js
@@ -3,15 +3,13 @@ var fs= requre('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var indexHtml=fs.readFileSync('index.html');
-  var string=indexHtml.toString('utf-8');
-  response.send(string);  
+ 
+  var buf=fs.readFileSync('index.html');
+  response.send(buf.toString('utf8'));  
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
-
-//response.send(fs.readFileSync("index.html").toString());
 
